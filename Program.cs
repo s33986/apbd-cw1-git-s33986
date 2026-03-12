a571a750977ecfe070e068de2e55e983bb37a9ed
Piotr Jasiński <s33986@pjwstk.edu.pl>
2026-03-12 15:28:11 +0100

    added countToTen method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main()
+    {
+        countToTen();
+    }
+
+
+    static void countToTen()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
